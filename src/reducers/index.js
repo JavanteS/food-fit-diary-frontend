addc9a5757ca45f1c171e4cef48605d697d7e5d1
javante_nelson <javantenelson@gmail.com>
2020-08-11 15:57:37 -0500

    add reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from "redux"
+import mealReducer from "./mealreducers"
+
+const rootReducers = combineReducers({
+    mealReducer
+})
---
+++ src/reducers/mealreducers.js
@@ -0,0 +1,12 @@
+
+export default (state ={ 
+    meals: [], loading: false
+}, action) =>{
+    switch(action.type){
+        case "ADD_MEAL":
+            
+
+        default:
+            return state
+    }
+}

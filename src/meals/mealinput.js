a26bc1f17d46f9acfa33bedf4e5d9396cda76ce5
javante_nelson <javantenelson@gmail.com>
2020-08-12 16:47:46 -0500

    add onchange event to form

--- src/meals/mealinput.js
+++ src/meals/mealinput.js
@@ -4,10 +4,16 @@ class MealInput extends Component {
 
 
     state={
+        name: "",
+        category: "",
+        calories: null
 
     }
 
-    handleChange = ()=>{
+    handleChange = (event)=>{
+        this.setState({
+            [event.target.name]: event.target.value
+        })
 
     }
 
@@ -15,13 +21,24 @@ class MealInput extends Component {
 
     }
 
-    
+
     render(){
         return (
-            <form>
-                <input type="text"/>
+
+            <div>
+               <form>
+                <label>Name</label>
+                <input type="text" onChange={this.handleChange} value={this.state.name} name="name"/>
+                
+                <label>Category</label>
+                <input type="text" onChange={this.handleChange} value={this.state.category} name="category"/>
+
+                <label>Calories</label>
+                <input type="text" onChange={this.handleChange} value={this.state.calories} name="calories"/>
                 <input type="submit"/>
-            </form>
+            </form>  
+            </div>
+           
         )
     }
 }

d1f047a7d2513ab4714adf598bfabb260943e491
javante_nelson <javantenelson@gmail.com>
2020-08-12 20:53:06 -0500

    add meals component to Meals container

--- src/containers/mealscontainer.js
+++ src/containers/mealscontainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MealInput from "../meals/mealinput"
+import Meals from "../meals/meals"
 
 
 class MealsContainer extends Component {
---
+++ src/meals/meals.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Meals extends Component {
+
+    render(){
+        return 1
+    }
+}
+
+export default Meals;
